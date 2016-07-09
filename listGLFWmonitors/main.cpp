3e88ab74c1137b3c082518b9263cf5060e02b7fa
Kuban Altan <kuban.altan@gmail.com>
2016-07-09 22:57:30 +0300

    - refactored the print functions to handle single mode line - added primary monitor check - added current active video mode for each monitor - added monitor position on desktop

--- listGLFWmonitors/main.cpp
+++ listGLFWmonitors/main.cpp
@@ -2,19 +2,25 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
-void printModes(const GLFWvidmode* modes, int modeCount)
+void printSingleMode(const GLFWvidmode mode)
+{
+    std::cout << mode.width << "x";
+    std::cout << mode.height <<"_";
+    std::cout << mode.refreshRate << "Hz ";
+    std::cout << "R" << mode.redBits;
+    std::cout << "G" << mode.greenBits;
+    std::cout << "B" << mode.blueBits;
+    
+    std::cout << std::endl;
+}
+
+void printModeArray(const GLFWvidmode* modes, int modeCount)
 {
     for (int i = 0; i < modeCount; ++i )
     {
         std::cout << "Mode #" << i << ": ";
-        std::cout << modes[i].width << "x";
-        std::cout << modes[i].height <<"_";
-        std::cout << modes[i].refreshRate << "Hz ";
-        std::cout << "R" << modes[i].redBits;
-        std::cout << "G" << modes[i].greenBits;
-        std::cout << "B" << modes[i].blueBits;
-        
-        std::cout << std::endl;
+        printSingleMode(modes[i]);
+
     }
 }
 
@@ -23,24 +29,47 @@ int main()
     // Init GLFW
     glfwInit();
     
+    GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+    
     int monitorCount;
     GLFWmonitor** monitors = glfwGetMonitors(&monitorCount);
     std::cout << "Number of detected monitors: " << monitorCount << std::endl;
     
     int widthMM, heightMM;
+    int xpos, ypos;
     int modeCount;
+    const char* monitorName;
     
     // Dump information for all the monitors detected by GLFW in this loop
-    for ( int i=0; i<monitorCount; ++i)
+    for ( int i=0; i < monitorCount; ++i)
     {
+        monitorName = glfwGetMonitorName(monitors[i]);
+        std::cout << "Monitor#" << i << " Name: " << monitorName;
+
+        // Indicate if this monitor is the "primary" monitor
+        if (monitors[i] == primaryMonitor)
+        {
+            std::cout << " (primary)";
+        }
+        std::cout << std::endl;
+        
+        // Get Monitors Desktop Position, the way it has been offset logically
+        glfwGetMonitorPos(monitors[i], &xpos, &ypos);
+        std::cout << "Monitor#" << i << " Position: " << xpos << "," << ypos << std::endl;
+        
         glfwGetMonitorPhysicalSize(monitors[i], &widthMM, &heightMM);
         std::cout << "Monitor#" << i << " Physical Size: " << widthMM << "mm. x " << heightMM << "mm." << std::endl;
-        
+
+        // Get currently active video mode for the current monitor
+        const GLFWvidmode* currentMode = glfwGetVideoMode(monitors[i]);
+        std::cout << "Monitor#" << i << " Active Mode: ";
+        printSingleMode(*currentMode);
+
         // Get information for monitor #i in terms of modes and count of modes
         const GLFWvidmode* modes = glfwGetVideoModes(monitors[i], &modeCount);
         std::cout << "Monitor#" << i << " Supported Video Modes: " << modeCount << std::endl;
         
-        printModes(modes, modeCount);
+        printModeArray(modes, modeCount);
     }
     
     // Terminates GLFW, clearing any resources allocated by GLFW.
